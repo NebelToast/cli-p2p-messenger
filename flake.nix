ce743ede0b2c4ec2e9fab3daa804e5a7900865c9
NebelToast <juliussteude@gmail.com>
2026-02-14 19:25:25 +0100

    feat(nix): add naersk build suppor

--- flake.nix
+++ flake.nix
@@ -2,20 +2,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     rust-overlay,
+    naersk, # 
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       overlays = [rust-overlay.overlays.default];
     };
+    
     toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+
+    naersk-lib = pkgs.callPackage naersk {
+      cargo = toolchain;
+      rustc = toolchain;
+    };
   in {
+    packages.${system}.default = naersk-lib.buildPackage {
+      root = ./.;
+
+    };
+
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         toolchain
@@ -25,8 +39,6 @@
         pkgs.git-cliff
       ];
 
-
-      # This is still useful for rust-analyzer
       RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
     };
   };
